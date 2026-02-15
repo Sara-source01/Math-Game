829e65891d35cba419f4fba3e9e3d73be19ea524
sara <saraebrahimgeorge@gmail.com>
2026-02-14 22:32:30 -0300

    feat: add core data structures for quiz and questions

--- Math Game.cpp
+++ Math Game.cpp
@@ -93,6 +93,30 @@ enOperationType ReadOpType()
     return (enOperationType) OpType;
 }
 
+struct stQuestion
+{
+    int Number1 = 0;
+    int Number2 = 0;
+    enOperationType OperationType;
+    enQuestionsLevel QuestionLevel;
+    int CorrectAnswer = 0;
+    int PlayerAnswer = 0;
+    bool AnswerResult = false;
+};
+
+
+struct stQuizz
+{
+    stQuestion QuestionList[100];
+    short NumberOfQuestions;
+    enQuestionsLevel QuestionsLevel;
+    enOperationType OpType;
+    short NumberOfWrongAnswers = 0;
+    short NumberOfRightAnswers = 0;
+    bool isPass = false;
+};
+
+
 
 int main()
 {
